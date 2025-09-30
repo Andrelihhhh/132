3bf2e491a0010110b5e7588bb825257b544eb068
Andrey <fkdkfkdf@mail.ru>
2025-09-30 18:50:18 +0300

    сделал приведение типов

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,33 @@
 import pandas as pd
 
 FILE_ID = "1j7ZAFLYStESJoCrkH0-bLctoxV75djUf"
-
 file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 
 print("Загрузка данных...")
 raw_data = pd.read_csv(file_url)
 
-print("\nПервые 10 строк датасета:")
-print(raw_data.head(10))
+data = raw_data.astype({
+    "Customer_ID": "string",
+    "Product_ID": "string",
+    "Transaction_ID": "string",
+    "Purchase_Frequency": "int64",
+    "Average_Order_Value": "float64",
+    "Most_Frequent_Category": "category",
+    "Time_Between_Purchases": "int64",
+    "Region": "category",
+    "Churn_Probability": "float64",
+    "Lifetime_Value": "float64",
+    "Season": "category",
+    "Preferred_Purchase_Times": "category",
+    "Retention_Strategy": "category"
+})
+
+data["Launch_Date"] = pd.to_datetime(data["Launch_Date"])
+data["Peak_Sales_Date"] = pd.to_datetime(data["Peak_Sales_Date"])
+
+print(data.info())
+
+output_file = "data.parquet"
+data.to_parquet(output_file, index=False)
+
+print(f"\nФайл сохранён: {output_file}")
